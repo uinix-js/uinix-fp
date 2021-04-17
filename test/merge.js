2a37a26a9f2633faa6bd44478c76f197c402934e
chrisrzhou <chrisrzhou@pm.me>
2021-04-16 22:51:19 -0700

    fix(merge): use deepmerge instead of lodash.merge to avoid mutations

--- test/merge.js
+++ test/merge.js
@@ -8,6 +8,15 @@ test('merge', (t) => {
       b: {
         c: 42,
       },
+      d: [1, 2, 3],
+    },
+  };
+  const x1Clone = {
+    a: {
+      b: {
+        c: 42,
+      },
+      d: [1, 2, 3],
     },
   };
   const x2 = {
@@ -15,9 +24,18 @@ test('merge', (t) => {
       b: {
         c: 24,
       },
-      d: {
-        e: 42,
+      d: [3, 2, 1],
+    },
+    f: {
+      g: 'h',
+    },
+  };
+  const x2Clone = {
+    a: {
+      b: {
+        c: 24,
       },
+      d: [3, 2, 1],
     },
     f: {
       g: 'h',
@@ -31,16 +49,18 @@ test('merge', (t) => {
         b: {
           c: 24,
         },
-        d: {
-          e: 42,
-        },
+        d: [3, 2, 1],
       },
       f: {
         g: 'h',
       },
     },
-    'should perform lodash.merge',
+    'should perform deepmerge with array overwrites',
   );
+
+  t.deepEqual(x1, x1Clone, 'should not mutate x1');
+
+  t.deepEqual(x2, x2Clone, 'should not mutate x2');
 
   t.end();
 });
