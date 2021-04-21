5fc69a3b8d5b5682522a0750bb64c42dda7bdd79
chrisrzhou <chrisrzhou@pm.me>
2021-04-20 23:18:59 -0700

    fix(prop): return undefined instead of throwing error if nested prop not found

--- test/prop.js
+++ test/prop.js
@@ -4,7 +4,11 @@ import { prop } from '../index.js';
 
 test('prop', (t) => {
   t.equal(prop('name')({ name: 'John' }), 'John');
-  t.equal(prop('name')({ age: 42 }), undefined);
+  t.equal(
+    prop('name')({ age: 42 }),
+    undefined,
+    'returns undefined if prop not found',
+  );
 
   const x = {
     a: {
@@ -16,7 +20,11 @@ test('prop', (t) => {
   t.equal(prop('a')(x), x.a, 'should shallow equal prop value');
   t.equal(prop('a.b.c')(x), 42, 'supports path accessor');
   t.equal(prop('a.b')(x), x.a.b, 'supports path accessor with shallow equal');
-  t.throws(() => prop('a.b.c.d.e.f', x), 'throws if cannot access nested property');
+  t.equal(
+    prop('a.b.c.d.e.f')(x),
+    undefined,
+    'returns undefined if nested prop not found',
+  );
 
   t.end();
 });
