2b895eab580af2ab7d449d3990cf4cfddd2e7c02
chrisrzhou <chrisrzhou@pm.me>
2021-11-25 23:01:53 -0800

    test: remove all tests

--- test/index.js
+++ test/index.js
@@ -1,9 +1,1 @@
-import './filter.js';
-import './i.js';
-import './is-plain-object.js';
-import './k.js';
 import './merge.js';
-import './noop.js';
-import './pipe.js';
-import './prop.js';
-import './props.js';
